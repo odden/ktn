2c4fba121b8dd0d59cbfe7fe45bef82503683cd6
Frida <sch_frida@hotmail.com>
2015-03-12 13:28:32 +0100

    Made some awsm changes.

--- Client.py
+++ Client.py
@@ -23,7 +23,7 @@ class Client:
     def run(self, host, server_port):
         # Initiate the connection to the server
         self.__init__()
-        print "Welcome to AwzmChat<3 write something awezome"
+        print "Welcome to AwzmChat<3 write something awezome - aand be awezome."
         self.connection.connect((self.host, self.server_port))
 
         self.login = False
@@ -31,13 +31,17 @@ class Client:
         while not login:
             self.username = raw_input('Username: ')
             self.login()
+            resp = self.connection.recv(1024).strip()
+            self.handle_json(resp)
 
-        self.threading = threading.Thread(target = self.receive_message)
-        self.threading.setDeamon(True)
-        self.threading.start()
+        self.thread = threading.Thread(target = self.receive_message)
+        self.thread.setDeamon(True)
+        self.thread.start()
 
         while login:
-            
+            receive_message()
+
+        self.connection.close()
 
 
     def login(self):
@@ -50,17 +54,45 @@ class Client:
 
     def receive_message(self, message):
         # TODO: Handle incoming message
-        while(login):
-            data = raw_input()
-
-            received = connection.recv(1024)
-            self.send(self.parse({'request':'msg', 'content' }))
-        pass
+        while():
+            received = self.connection.recv(1024).strip()
+            self.process_json(received)            
 
     def send_payload(self, data):
         # TODO: Handle sending of a payload
+        data = raw_input("Type something: ")
         self.connection.sendall(data)
-        pass
+        self.send(self.parse({'request':'msg', 'content' }))
+
+    def parse(self, data):
+        return json.dumps(data)
+
+    def handle_json(self, data):
+        index = 0
+        while data.find("{", index) >= 0:
+            start = data.find("{", index)
+            end = data.find("}", start)
+            index = end
+            self.handle_data(data[start:end+1])
+    
+    def handle_data(self, data):
+        decoded = json.loads(data)
+        if decoded.get("response", "") == "login":
+            if decoded.get("error", "") != "":
+                print decoded["error"], "(%s)"%decoded.get("username", "")
+            else:
+                self.logged_in = True
+            if decoded.get("messages", "") != "":
+                print decoded["messages"].encode('utf-8')
+
+        if not self.logged_in:
+            return
+        
+        if decoded.get("response", "") == "logout":
+            self.logged_in = False
+
+        if decoded.get("response", "") == "message":
+            print decoded["message"].encode('utf-8')
 
 
 if __name__ == '__main__':
